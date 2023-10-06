1595094a7dfc02664f94c86b36b1e3e555a091b0
DevExpressExampleBot <fakeEmail>
2023-10-06 11:56:00 +0400

    Source auto update [skip ci]

--- VB/WindowsFormsApplication1/Form1.vb
+++ VB/WindowsFormsApplication1/Form1.vb
@@ -1,101 +1,97 @@
-﻿Imports System
-Imports System.Collections.Generic
+Imports System
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraEditors.Repository
 Imports DevExpress.XtraEditors
 Imports DevExpress.XtraSpreadsheet
 Imports DevExpress.Spreadsheet
 
-
 Namespace WindowsFormsApplication1
-	Partial Public Class Form1
-		Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-			Dim rePopupContainer As RepositoryItemPopupContainerEdit = TryCast(propertyGridControl1.RepositoryItems.Add("PopupContainerEdit"), RepositoryItemPopupContainerEdit)
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			rePopupContainer.PopupControl = popupContainerControl1
-			AddHandler rePopupContainer.QueryPopUp, AddressOf rePopupContainer_QueryPopUp
-			AddHandler rePopupContainer.QueryResultValue, AddressOf rePopupContainer_QueryResultValue
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            Dim rePopupContainer As RepositoryItemPopupContainerEdit = TryCast(propertyGridControl1.RepositoryItems.Add("PopupContainerEdit"), RepositoryItemPopupContainerEdit)
+            rePopupContainer.PopupControl = popupContainerControl1
+            AddHandler rePopupContainer.QueryPopUp, New CancelEventHandler(AddressOf rePopupContainer_QueryPopUp)
+            AddHandler rePopupContainer.QueryResultValue, New DevExpress.XtraEditors.Controls.QueryResultValueEventHandler(AddressOf rePopupContainer_QueryResultValue)
+            propertyGridControl1.DefaultEditors.Add(GetType(DataTable), rePopupContainer)
+            AddHandler rePopupContainer.CustomDisplayText, New DevExpress.XtraEditors.Controls.CustomDisplayTextEventHandler(AddressOf rePopupContainer_CustomDisplayText)
+            Dim newObject As MyObject = New MyObject()
+            propertyGridControl1.SelectedObject = newObject
+            gridControl1.DataSource = New BindingList(Of MyObject)() From {newObject}
+            gridControl1.ForceInitialize()
+            gridView1.Columns("MySpreadSheet").ColumnEdit = rePopupContainer
+        End Sub
 
-			propertyGridControl1.DefaultEditors.Add(GetType(DataTable), rePopupContainer)
-			AddHandler rePopupContainer.CustomDisplayText, AddressOf rePopupContainer_CustomDisplayText
+        Private Sub rePopupContainer_CustomDisplayText(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.Controls.CustomDisplayTextEventArgs)
+            e.DisplayText = "(table data)"
+        End Sub
 
-			Dim newObject As New MyObject()
+        Private Sub rePopupContainer_QueryResultValue(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.Controls.QueryResultValueEventArgs)
+            Dim currentTable As DataTable = TryCast(propertyGridControl1.SelectedObject, MyObject).MySpreadSheet
+            If currentTable Is Nothing Then
+                currentTable = New DataTable()
+                TryCast(propertyGridControl1.SelectedObject, MyObject).MySpreadSheet = currentTable
+            End If
 
-			propertyGridControl1.SelectedObject = newObject
-			gridControl1.DataSource = New BindingList(Of MyObject)() From {newObject}
-			gridControl1.ForceInitialize()
+            currentTable.Clear()
+            currentTable.Columns.Clear()
+            Dim spreadSheet As SpreadsheetControl = TryCast(TryCast(sender, PopupContainerEdit).Properties.PopupControl.Controls(0), SpreadsheetControl)
+            Dim workSheet As Worksheet = spreadSheet.Document.Worksheets(0)
+            Dim usedRange As CellRange = workSheet.GetUsedRange()
+            For i As Integer = 0 To usedRange.ColumnCount - 1
+                currentTable.Columns.Add("Column" & i.ToString())
+            Next
 
-			gridView1.Columns("MySpreadSheet").ColumnEdit = rePopupContainer
-		End Sub
+            For i As Integer = 0 To usedRange.RowCount - 1
+                Dim newRow As DataRow = currentTable.NewRow()
+                For j As Integer = 0 To usedRange.ColumnCount - 1
+                    newRow(j) = workSheet.Cells(i, j).DisplayText
+                Next
 
-		Private Sub rePopupContainer_CustomDisplayText(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.Controls.CustomDisplayTextEventArgs)
-			e.DisplayText = "(table data)"
-		End Sub
+                currentTable.Rows.Add(newRow)
+            Next
+        End Sub
 
-		Private Sub rePopupContainer_QueryResultValue(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.Controls.QueryResultValueEventArgs)
-			Dim currentTable As DataTable = (TryCast(propertyGridControl1.SelectedObject, MyObject)).MySpreadSheet
-			If currentTable Is Nothing Then
-				currentTable = New DataTable()
-				TryCast(propertyGridControl1.SelectedObject, MyObject).MySpreadSheet = currentTable
-			End If
-			currentTable.Clear()
-			currentTable.Columns.Clear()
-			Dim spreadSheet As SpreadsheetControl = TryCast((TryCast(sender, PopupContainerEdit)).Properties.PopupControl.Controls(0), SpreadsheetControl)
-			Dim workSheet As Worksheet = spreadSheet.Document.Worksheets(0)
-			Dim usedRange As CellRange = workSheet.GetUsedRange()
+        Private Sub rePopupContainer_QueryPopUp(ByVal sender As Object, ByVal e As CancelEventArgs)
+            Dim currentTable As DataTable = TryCast(propertyGridControl1.SelectedObject, MyObject).MySpreadSheet
+            If currentTable IsNot Nothing Then
+                Dim spreadSheet As SpreadsheetControl = TryCast(TryCast(sender, PopupContainerEdit).Properties.PopupControl.Controls(0), SpreadsheetControl)
+                Dim workSheet As Worksheet = spreadSheet.Document.Worksheets(0)
+                For i As Integer = 0 To currentTable.Columns.Count - 1
+                    For j As Integer = 0 To currentTable.Rows.Count - 1
+                        workSheet.Cells(j, i).Value = currentTable.Rows(j)(i).ToString()
+                    Next
+                Next
+            End If
+        End Sub
+    End Class
 
-			For i As Integer = 0 To usedRange.ColumnCount - 1
-				currentTable.Columns.Add("Column" & i.ToString())
-			Next i
+    Public Class MyObject
 
-			For i As Integer = 0 To usedRange.RowCount - 1
-				Dim newRow As DataRow = currentTable.NewRow()
-				For j As Integer = 0 To usedRange.ColumnCount - 1
-					newRow(j) = workSheet.Cells(i,j).DisplayText
-				Next j
-				currentTable.Rows.Add(newRow)
-			Next i
-		End Sub
+        Public Sub New()
+            Name = "Some name"
+            MySpreadSheet = New DataTable()
+            For i As Integer = 0 To 5 - 1
+                MySpreadSheet.Columns.Add("Value" & i.ToString(), GetType(Integer))
+            Next
 
-		Private Sub rePopupContainer_QueryPopUp(ByVal sender As Object, ByVal e As CancelEventArgs)
-			Dim currentTable As DataTable = (TryCast(propertyGridControl1.SelectedObject, MyObject)).MySpreadSheet
-			If currentTable IsNot Nothing Then
-				Dim spreadSheet As SpreadsheetControl = TryCast((TryCast(sender, PopupContainerEdit)).Properties.PopupControl.Controls(0), SpreadsheetControl)
-				Dim workSheet As Worksheet = spreadSheet.Document.Worksheets(0)
-				For i As Integer = 0 To currentTable.Columns.Count - 1
-					For j As Integer = 0 To currentTable.Rows.Count - 1
-						workSheet.Cells(j, i).Value = currentTable.Rows(j)(i).ToString()
-					Next j
-				Next i
-			End If
-		End Sub
-	End Class
+            Dim myRand As Random = New Random()
+            For i As Integer = 0 To 20 - 1
+                MySpreadSheet.Rows.Add(myRand.Next(1, 100), myRand.Next(1, 100), myRand.Next(1, 100), myRand.Next(1, 100), myRand.Next(1, 100))
+            Next
+        End Sub
 
-	Public Class MyObject
-		Public Sub New()
-			Name = "Some name"
-			MySpreadSheet = New DataTable()
-			For i As Integer = 0 To 4
-				MySpreadSheet.Columns.Add("Value" & i.ToString(), GetType(Integer))
-			Next i
-			Dim myRand As New Random()
-			For i As Integer = 0 To 19
-				MySpreadSheet.Rows.Add(myRand.Next(1, 100), myRand.Next(1, 100), myRand.Next(1, 100), myRand.Next(1, 100), myRand.Next(1, 100))
-			Next i
-		End Sub
+        Public Property Name As String
 
-		Public Property Name() As String
-		Public Property MySpreadSheet() As DataTable
-	End Class
+        Public Property MySpreadSheet As DataTable
+    End Class
 End Namespace
